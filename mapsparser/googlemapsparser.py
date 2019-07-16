9c85270d266436e489bea22cba39546c751aedbd
Vitaly Korneev <vitaly_korneev@tut.by>
2019-07-16 21:56:20 +0300

    Added GoogleMapsParser

---
+++ mapsparser/googlemapsparser.py
@@ -0,0 +1,41 @@
+"""Google maps parser"""
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
+
+from .constants import Const
+from .mapsparser import MapsParser
+from .googlepages import MapsPage
+
+
+class GoogleMapsParser(MapsParser):
+    def __init__(self):
+        options = FirefoxOptions()
+        options.add_argument(Const.FIREFOX_DEFAULT_WINDOW_SIZE)
+        self._driver = webdriver.Firefox(firefox_options=options,
+                                         executable_path=Const.EXECUTABLE_PATH)
+        self._maps_page = MapsPage(self._driver)
+
+    def find_travel_time(self, starting_address: str,
+                         destination_address: str) -> None:
+        # maximize directions panel
+        self._maps_page.click_search_box_directions_button()
+
+        # insert starting and destination points
+        starting_address_string = f"{starting_address}\n"
+        self._maps_page.insert_starting_address(starting_address_string)
+        destination_address_string = f"{destination_address}\n"
+        self._maps_page.insert_destination_address(destination_address_string)
+
+        # select travel mode by bus-trolleybus
+        self._maps_page.click_travel_mode_transit_button()
+
+        self._maps_page.click_select_schedule_button()
+        self._maps_page.click_depart_menu_item()
+
+        # select default time
+        self._maps_page.click_time_picker_field()
+        self._maps_page.click_default_time_div()
+
+    def quit(self) -> None:
+        self._driver.close()
+        self._driver.quit()
