c1fca031bd98fb0d5e187752ba0e4225360a876a
Vitaly Korneev <vitaly_korneev@tut.by>
2019-07-16 21:56:36 +0300

    Added constants class

---
+++ mapsparser/constants.py
@@ -0,0 +1,13 @@
+class Const:
+    FIREFOX_DEFAULT_WINDOW_SIZE = "--window-size=1024,768"
+    EXECUTABLE_PATH = r'./mapsparser/drivers/geckodriver'
+    GOOGLE_MAPS_URL = "https://www.google.com/maps/"
+    GOOGLE_MAPS_PAGE_TITLE = "Google Maps"
+    SEARCHBOX_DIRECTIONS_BUTTON_ID = ".searchbox-directions"
+    STARTING_ADDRESS_FIELD_ID = "#sb_ifc51 input"
+    DESTINATION_ADDRESS_FIELD_ID = "#sb_ifc52 input"
+    TRAVEL_MODE_TRANSIT_BUTTON_ID = '[aria-label="Transit"]'
+    SELECT_SCHEDULE_BUTTON_ID = "#\:4"
+    DEPART_MENU_ITEM_ID = "#\:1 div"
+    TIME_INPUT_ID = ".time-input"
+    DEFAULT_TIME_DIV_ID = "#\:y .goog-menuitem-content"
