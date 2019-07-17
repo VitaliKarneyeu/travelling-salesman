09c0fb5a85effae3a1527dd7f27a224e5a1b9bbd
Vitaly Korneev <vitaly_korneev@tut.by>
2019-07-17 18:13:19 +0300

    changed search interface

--- searcher/search.py
+++ searcher/search.py
@@ -4,8 +4,10 @@ from abc import abstractmethod
 
 class Search(metaclass=ABCMeta):
     def __init__(self, times_table: list, points: list) -> None:
-        self.search_fastest_way(times_table, points)
+        self._time_table = times_table
+        self._points = points
+        self.search_fastest_way()
 
     @abstractmethod
-    def search_fastest_way(self, times_table: list, points: list) -> None:
+    def search_fastest_way(self) -> None:
         pass
